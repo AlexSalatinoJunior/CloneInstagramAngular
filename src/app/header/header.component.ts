be4a9d31522a70e303deedd1284f404413695639
Alex Salatino Junior <alex.s.jr@outlook.com>
2022-07-30 21:42:06 -0300

    Header finished, but with responsivity issues

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', '../../../node_modules/normalize.css/normalize.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  ngOnInit(){
   }
 
+  focarInput() {
+    var search = document.getElementById('search')
+    var textArea = document.getElementById("textarea") as HTMLInputElement
+    if(search != null)
+      search.style.display = 'none'
+    if(textArea != null){
+      textArea.style.color = 'black'
+      textArea.select()
+    }
+  }
+  desfocarInput(){
+    var search = document.getElementById('search')
+    var textArea = document.getElementById("textarea")
+    if(search != null){
+      search.style.display = 'block'
+    }
+    if(textArea != null){
+      textArea.style.color = 'rgb(170, 170, 170)'
+    }
+  }
+  abreInput(){
+    var input = document.getElementById("textarea")
+    input?.focus()
+  }
 }
